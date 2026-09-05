1d0ad3af41dc9b45dd7e56626e94e0e2b8981935
ji <user@dell-void>
2026-09-04 22:15:51 -0400

    fix dwmblocks to not show blocked time as cpu time

--- blocks.def.h
+++ blocks.def.h
@@ -1,11 +1,16 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	{"  ", "date '+%a %b %d %I:%M%p'",                                  1,      0},
+	{"󰖀 ", "wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{printf \"%d%%\", $2 * 100; if ($3 == \"[MUTED]\") printf \" muted\"; print \"\"}'", 1,0},
+	{"", "dwmblocks-wifi", 1, 0},
+    {"CPU ", "top -bn1 | awk '/Cpu/ {print int($2+$4+$6+$12+$14+$16) \"%\"}'", 5, 0},     
+	{"", "free --mega | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",     5,     0},
+    // {"", "awk '{printf \"%s%% \", $1}' /sys/class/power_supply/BAT0/capacity; awk '{printf \"%s \", $1 $2}' /sys/class/power_supply/BAT0/status", 10, 0},
+    {"", "dwmblocks-charge-rate", 10, 0},
 };
 
-//sets delimiter between status commands. NULL character ('\0') means no delimiter.
+# 
+// sets delimiter between status commands. NULL character ('\0') means no delimiter.
 static char delim[] = " | ";
 static unsigned int delimLen = 5;
--- blocks.h
+++ blocks.h
@@ -4,7 +4,7 @@ static const Block blocks[] = {
 	{"  ", "date '+%a %b %d %I:%M%p'",                                  1,      0},
 	{"󰖀 ", "wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{printf \"%d%%\", $2 * 100; if ($3 == \"[MUTED]\") printf \" muted\"; print \"\"}'", 1,0},
 	{"", "dwmblocks-wifi", 1, 0},
-    {"CPU ", "top -bn1 | awk '/Cpu/ {print int(100 - $8) \"%\"}'", 5, 0},
+    {"CPU ", "top -bn1 | awk '/Cpu/ {print int($2+$4+$6+$12+$14+$16) \"%\"}'", 5, 0},     
 	{"", "free --mega | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",     5,     0},
     // {"", "awk '{printf \"%s%% \", $1}' /sys/class/power_supply/BAT0/capacity; awk '{printf \"%s \", $1 $2}' /sys/class/power_supply/BAT0/status", 10, 0},
     {"", "dwmblocks-charge-rate", 10, 0},
